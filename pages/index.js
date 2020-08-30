46ea962c90833aab87b742a6653a99d49aefaff1
fluorspar20 <amoghdixit104@gmail.com>
2020-08-30 19:26:52 +0530

    updated my-info in index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello World! I am <span style={{color: 'blueviolet'}}>Amogh Dixit</span>, a sophomore pursuing <span style={{color: 'blueviolet'}}>Electrical Engineering</span> from the <span style={{color: 'blueviolet'}}>Indian Institute of Technology, Indore</span>. I am passionate about programming and I am learning full stack web development using MERN stack.</p>
+        <p>Hello World! I am <span style={{color: 'blueviolet'}}>Amogh Dixit</span>, a sophomore pursuing <span style={{color: 'blueviolet'}}>Electrical Engineering</span> from the <span style={{color: 'blueviolet'}}>Indian Institute of Technology, Indore</span>. I am passionate about programming and I am learning full stack web development using MERN.</p>
         <p>Intrigued by travelling, art, music, movies, sports and nature, I am also a hobbyist photographer and I love capturing moments.</p>
         <p>You can contact me on <a href="https://twitter.com/runtimeError20" target="_blank">Twitter</a></p>
         {/* <p>
